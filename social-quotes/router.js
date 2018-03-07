e4736d0c50743deac734aab230376f134a5cbfb0
Daniel-Slabaugh <danielslabaugh@gmail.com>
2018-03-06 16:15:17 -0800

    added put method for social-quotes

--- social-quotes/router.js
+++ social-quotes/router.js
@@ -86,6 +86,43 @@ router.post('/',
         });
 });
 
+router.put('/:id',
+  passport.authenticate('jwt', {session: false}), 
+  (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
 
+  const toUpdate = {};
+  const updateableFields = ['quote', 'reference', 'tags'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Quote
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(quote => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+router.delete('/:id',
+  passport.authenticate('jwt', {session: false}), 
+  (req, res) => {
+  Sailboat
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(sailboat => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
 
 module.exports = {router};
